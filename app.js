57169ab74f7b840e8258ed19b47b2d431109b39b
jongchang <hokhmah91@naver.com>
2023-11-07 04:51:04 +0900

    test04

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const commentsRouter = require("./routes/comments.route");
 const app = express();
 const PORT = 8000;
 
+const cors = require("cors");
+let corsOptions = {
+    origin: "*", // 출처 허용 옵션
+    credential: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+};
+app.use(cors(corsOptions));
+
 app.use(cookieParser());
 app.use(express.json());
 
